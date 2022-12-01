9d3f2f65da994dcf5b5b37e92d7b18a79c87d9d8
dung.lt.cntt <dung.lt.cntt@becamex.com.vn>
2022-12-01 15:26:00 +0700

    update cron job backup for each week on sunday

--- sources/apps/velero/scripts/1.schedule.sh
+++ sources/apps/velero/scripts/1.schedule.sh
@@ -8,7 +8,7 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 namespace_array=(kubeapps sso-dev gis-uat fast esign-uat customer-portal-uat becamex-mobile-uat becamex-xlnt-uat becamex-hospital-uat becamex-digimaps becamex-crm-uat becamex-apps-dev)
-cron_job="@every 1w"
+cron_job="0 1 * * SUN" #At 01:00 on Sunday
 echo -e "${YELLOW}===== Begin make Schedule =====${NC}"
 for i in ${namespace_array[@]}
 do
